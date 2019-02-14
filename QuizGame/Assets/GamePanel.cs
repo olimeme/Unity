a99ba4767710392e9257eecb183637618553d505
olimeme <alibek.mombekov@mail.ru>
2019-02-14 17:53:52 +0600

    Front End

---
+++ QuizGame/Assets/GamePanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePanel : MonoBehaviour
+{
+    [SerializeField]
+	private Button trueBtn, falseBtn;
+
+    [SerializeField]
+	private Text questionText;
+
+    void Start()
+    {
+        trueBtn.onClick.AddListener(delegate{SwitchButtonHandler(0)});
+		falseBtn.onClick.AddListener(delegate{SwitchButtonHandler(1)});
+    }
+
+    public void Init(string message){
+		questionText.text = message;
+	}
+
+    void SwitchButtonHandler(int idx)
+    {
+    }
+}
